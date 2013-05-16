edd47eb763d09df08e8ace9efdffe51c9b50444b
Darren Hardy <drh@stanford.edu>
2013-05-16 14:35:05 -0700

    Add debug printout

--- lib/rgeoserver/geoserver_url_helpers.rb
+++ lib/rgeoserver/geoserver_url_helpers.rb
@@ -1,16 +1,23 @@
 
 module RGeoServer
   module GeoServerUrlHelpers
-    API_DOCUMENTATION = "http://docs.geoserver.org/latest/en/user/restconfig/rest-config-api.html"
-  
+    # See http://docs.geoserver.org/latest/en/user/rest/api/
+    # @return [String] baseURL for REST API, e.g.,:
+    # - layers/_name_.xml
+    # - styles/_name_.xml
+    # - workspaces/_name_.xml
+    # - namespaces/_name_.xml
+    # - workspaces/_name_/datastores/_name_.xml
+    # - workspaces/_name_/datastores/_name_/featuretype/_name_.xml
     def url_for base, options = nil
       base = { base => nil } unless base.is_a? Hash
       format = options.delete(:format) || 'xml'
       new_base = base.map{ |key,value|  value.nil?? key.to_s : [key.to_s, CGI::escape(value.to_s)].join("/")  }.join("/") 
       new_base = new_base.gsub(/\/$/,'')
       new_base += ".#{format}"
-      "#{new_base}" + (("?#{options.map { |key, value|  "#{CGI::escape(key.to_s)}=#{CGI::escape(value.to_s)}"}.join("&")  }" if options and not options.empty?) || '')
-
+      new_base += (("?#{options.map { |key, value|  "#{CGI::escape(key.to_s)}=#{CGI::escape(value.to_s)}"}.join("&")  }" if options and not options.empty?) || '')
+      # ap({:base => base, :new_base => new_base }) if $DEBUG
+      new_base
     end
 
   end
