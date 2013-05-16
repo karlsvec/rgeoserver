715a7a4e3606fd6f4c5db84e1c591af4069ce221
Darren Hardy <drh@stanford.edu>
2013-05-16 14:32:49 -0700

    Use RGEOSERVER_CONFIG

--- lib/rgeoserver/config.rb
+++ lib/rgeoserver/config.rb
@@ -1,7 +1,12 @@
 require 'confstruct/configuration'
 
+# Uses config/defaults.yml or config/$RGEOSERVER_CONFIG from environment
+# See https://rubygems.org/gems/confstruct for details on file format
 module RGeoServer
-  Config = Confstruct::Configuration.new(YAML.load(File.read(File.expand_path('../../../config/config_defaults.yml', __FILE__))))
+  Config = Confstruct::Configuration.new(
+    YAML.load(
+      File.read(
+        File.join(File.dirname(__FILE__), '..', '..', 'config', (ENV['RGEOSERVER_CONFIG'] ||= 'defaults.yml')))))
 end
 
 
