90c9ecd8908f2229180a247e92bb62c912881f61
Darren Hardy <drh@stanford.edu>
2013-06-03 00:23:03 -0700

    minor cleanup

--- lib/rgeoserver.rb
+++ lib/rgeoserver.rb
@@ -9,27 +9,27 @@ require 'time'
 module RGeoServer
   require 'rgeoserver/version'
   
-  autoload :Config, "rgeoserver/config"
-  autoload :Catalog, "rgeoserver/catalog"
-  autoload :RestApiClient, "rgeoserver/rest_api_client"
-  autoload :GeoServerUrlHelpers, "rgeoserver/geoserver_url_helpers"
-  autoload :ResourceInfo, "rgeoserver/resource"
-  autoload :Namespace, "rgeoserver/namespace"
-  autoload :Workspace, "rgeoserver/workspace"
-  autoload :FeatureType, "rgeoserver/featuretype"
-  autoload :Coverage, "rgeoserver/coverage"
-  autoload :DataStore, "rgeoserver/datastore"
-  autoload :CoverageStore, "rgeoserver/coveragestore"
-  autoload :WmsStore, "rgeoserver/wmsstore"
-  autoload :Style, "rgeoserver/style"
-  autoload :Layer, "rgeoserver/layer"
-  autoload :LayerGroup, "rgeoserver/layergroup"
+  autoload :Catalog,              "rgeoserver/catalog"
+  autoload :Config,               "rgeoserver/config"
+  autoload :Coverage,             "rgeoserver/coverage"
+  autoload :CoverageStore,        "rgeoserver/coveragestore"
+  autoload :DataStore,            "rgeoserver/datastore"
+  autoload :FeatureType,          "rgeoserver/featuretype"
+  autoload :GeoServerUrlHelpers,  "rgeoserver/geoserver_url_helpers"
+  autoload :Layer,                "rgeoserver/layer"
+  autoload :LayerGroup,           "rgeoserver/layergroup"
+  autoload :Namespace,            "rgeoserver/namespace"
+  autoload :ResourceInfo,         "rgeoserver/resource"
+  autoload :RestApiClient,        "rgeoserver/rest_api_client"
+  autoload :Style,                "rgeoserver/style"
+  autoload :WmsStore,             "rgeoserver/wmsstore"
+  autoload :Workspace,            "rgeoserver/workspace"
 
-  autoload :BoundingBox, "rgeoserver/utils/boundingbox"
-  autoload :ShapefileInfo, "rgeoserver/utils/shapefile_info"
-  autoload :Metadata, "rgeoserver/utils/metadata"
+  autoload :BoundingBox,          "rgeoserver/utils/boundingbox"
+  autoload :Metadata,             "rgeoserver/utils/metadata"
+  autoload :ShapefileInfo,        "rgeoserver/utils/shapefile_info"
 
-  # @return the default GeoServer Catalog instance
+  # @return [Catalog] the default GeoServer Catalog instance
   def self.catalog
     @@catalog ||= Catalog.new RGeoServer::config[:geoserver]
   end
