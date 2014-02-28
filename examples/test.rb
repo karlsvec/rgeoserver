7fb2130c0bae4f2df964407624aeec4a2781e903
Darren Hardy <drh@stanford.edu>
2014-02-28 07:39:25 -0800

    .

--- examples/test.rb
+++ examples/test.rb
@@ -1,37 +1,46 @@
 require 'awesome_print'
+require 'active_support'
+
+class String
+  def from_json
+    ActiveSupport::JSON.decode(self.to_s)
+  end
+end
+
+'{"hi":"hello"}'.from_json
 
 c = RGeoServer.catalog
 ap({:c => c})
 
 w = c.get_workspace 'druid'
 ap({:w => w})
-ap({:message => ActiveSupport::JSON.decode(w.message)})
+ap({:message => w.message.from_json})
 
 w.datastores do |ds|
   ap({:ds => ds})
-  ap({:message => ActiveSupport::JSON.decode(ds.message)})
+  ap({:message => ds.message.from_json})
 end
 
 ds = w.get_datastore 'geoserver_20140205_villagemaps'
 ap({:ds => ds})
-ap({:message => ActiveSupport::JSON.decode(ds.message)})
+ap({:message => json_decode(ds.message)})
 
 ds.featuretypes do |ft|
   ap({:ft => ft})
-  ap({:message => ActiveSupport::JSON.decode(ft.message)})
+  ap({:message => json_decode(ft.message)})
 end
 
 w.coveragestores do |cs|
   ap({:cs => cs})
-  ap({:message => ActiveSupport::JSON.decode(cs.message)})
+  ap({:message => json_decode(cs.message)})
 end
 
 cs = w.get_coveragestore 'nw926np8508'
 ap({:cs => cs})
-ap({:message => ActiveSupport::JSON.decode(cs.message)})
+ap({:message => json_decode(cs.message)})
 
-cs.coverages do |c|
-  ap({:c => c})
-  ap({:message => ActiveSupport::JSON.decode(c.message)})
+cs.coverages do |cv|
+  ap({:cv => cv})
+  ap({:message => json_decode(cv.message)})
 end
 
--- lib/rgeoserver/model/style.rb
+++ lib/rgeoserver/model/style.rb
@@ -5,43 +5,12 @@ module RGeoServer
     
     # attr_accessors
     # @see http://geoserver.org/display/GEOS/Catalog+Design
-    OBJ_ATTRIBUTES = {:catalog => 'catalog', :name => 'name', :sld_version => 'sldVersion', :filename => 'filename', :sld_doc => 'sld_doc' }
-    OBJ_DEFAULT_ATTRIBUTES = {:catalog => nil, :name => nil, :sld_version => nil, :filename => '', :sld_doc => nil }
-    
-    define_attribute_methods OBJ_ATTRIBUTES.keys
+    OBJ_ATTRIBUTES = %w{name filename sld_version sld_doc abstract title}
+    OBJ_DEFAULT_ATTRIBUTES = {}
+
+    define_attribute_methods OBJ_ATTRIBUTES
     update_attribute_accessors OBJ_ATTRIBUTES
-    # 
-    # @@sld_namespace = 'http://www.opengis.net/sld'
-    # 
-    # def sld_namespace
-    #   @@sld_namespace
-    # end
-    # 
-    # def create_options
-    #   {
-    #     :headers => {
-    #       :accept => :xml,
-    #       :content_type=> "application/vnd.ogc.sld+xml"
-    #     },
-    #     :format => :xml,
-    #     :name => name
-    #   }
-    # end   
-    # 
-    # def update_options
-    #   {
-    #     :headers => {
-    #       :accept => :xml,
-    #       :content_type=> "application/vnd.ogc.sld+xml"
-    #     },
-    #     :format => :sld
-    #   }
-    # end
-    # 
-    # def message
-    #   @sld_doc
-    # end
-    
+
     # 
     # @param [RGeoServer::Catalog] catalog
     # @param [Hash] options
@@ -52,30 +21,6 @@ module RGeoServer
         @name = options[:name].to_s.strip
       end
     end
-    
-    # 
-    # # Obtain all layers that use this style.
-    # # WARNING: This will be slow and inneficient when the list of all layers is too long.
-    # def layers
-    #   catalog.layers do |l|
-    #     lyrs = [l.profile['default_style']]+l.profile['alternate_styles']
-    #     yield l if lyrs.include? name
-    #   end 
-    # end
-    # 
-    # def profile_xml_to_hash profile_xml
-    #   doc = profile_xml_to_ng profile_xml
-    #   {
-    #     'name' => doc.at_xpath('//name').text.strip, 
-    #     'sld_version' => doc.at_xpath('//sldVersion/version').text,
-    #     'filename' => doc.at_xpath('//filename').text,
-    #     'sld_doc' => begin
-    #       Nokogiri::XML(catalog.search({:styles => @name}, options={:format => 'sld'})).to_xml
-    #     rescue RestClient::ResourceNotFound
-    #       nil 
-    #     end
-    #   }.freeze 
-    # end
 
   end
 end 
